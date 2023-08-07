6c3387b4ddbca5f96b69cd3d3199c4fdca325348
blade <zzy_geo@163.com>
2023-08-08 00:24:13 +0800

    增加spring-boot-start-jdbc

--- amdin/src/main/java/com/zzy/Hunan3dApplication.java
+++ amdin/src/main/java/com/zzy/Hunan3dApplication.java
@@ -1,8 +1,9 @@
 package com.zzy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication
 public class Hunan3dApplication {
     public static void main(String[] args) {
