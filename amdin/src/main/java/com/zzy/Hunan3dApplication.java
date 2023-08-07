b849b696d39b932e3d25a28fb76c46ecc36c7362
Blade <68823394+zzygeo@users.noreply.github.com>
2023-08-07 21:48:04 +0800

    初始化后台

---
+++ amdin/src/main/java/com/zzy/Hunan3dApplication.java
@@ -0,0 +1,11 @@
+package com.zzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hunan3dApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Hunan3dApplication.class, args);
+    }
+}
