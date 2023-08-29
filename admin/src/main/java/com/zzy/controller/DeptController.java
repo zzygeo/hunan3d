bfb4cb6aeb434459bb73127f1dad578b8abe1413
Blade <68823394+zzygeo@users.noreply.github.com>
2023-08-29 18:55:58 +0800

    controller增加

--- admin/src/main/java/com/zzy/controller/DeptController.java
+++ admin/src/main/java/com/zzy/controller/DeptController.java
@@ -1,4 +1,47 @@
 package com.zzy.controller;
 
+import com.zzy.domain.Dept;
+import com.zzy.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/dept")
+@Api(value = "部门相关")
 public class DeptController {
+    // 所有接口进行菜单权限控制
+    @PostMapping
+    @ApiOperation(value = "添加部门")
+    private Result addDept(@RequestBody Dept dept) {
+        // 判断部门是否存在，部门名称
+        return null;
+    }
+
+    @DeleteMapping
+    @ApiOperation(value = "通过id删除部门")
+    private Result deleteDept(@RequestParam(value = "deptId") Long deptId) {
+        return null;
+    }
+
+    @PostMapping("/update")
+    @ApiOperation(value = "更新部门")
+    private Result update(@RequestBody Dept dept) {
+        return null;
+    }
+    @PostMapping("/updateStatus")
+    @ApiOperation(value = "更新部门状态")
+    private Result updateStatus(@RequestParam(value = "deptId") Long deptId, @RequestParam(value = "status") String status) {
+        return null;
+    }
+    @GetMapping
+    @ApiOperation(value = "根据部门id查询")
+    private Result selectById(@RequestParam(value = "deptId") Long deptId) {
+        return null;
+    }
+    @GetMapping("/tree")
+    @ApiOperation(value = "查询树状结构的部门")
+    private Result selectTree() {
+        return null;
+    }
 }
